424ec68832898b55cc0ff871a20d9c08adff54f6
RubensSantos00 <rubenscvsantos@gmail.com>
2021-07-19 22:23:07 -0300

    Divisão do background e da caixa do chat

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -6,18 +6,32 @@ export default class App extends React.Component {
   render() {
 
     const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: aliceblue;
+    height: 100vh;
+   `
+
+
+    const ChatBox = styled.div`
       display: flex;
       flex-direction: column;
       width: 900px;
-      height: 900px;
+      height: 95%;
       border: solid;
-      margin: 15px auto auto auto
+      background-color: white;
+      margin: 15px 0 15px 0;
     `
 
 
     return (
       <Wrapper>
-        
+
+        <ChatBox>
+
+        </ChatBox>
+
       </Wrapper>
 
     )
